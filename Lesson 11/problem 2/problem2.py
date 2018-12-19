f2d9ac56790e9a652967d3ead22eaa1b3a4ce025
browngirlangie <44147244+browngirlangie@users.noreply.github.com>
2018-12-19 12:42:20 -0500

    Add files via upload

---
+++ Lesson 11/problem 2/problem2.py
@@ -0,0 +1,10 @@
+print('_'*65)
+print('Narrative Bot')
+print()
+name = input('What is your name?')
+name = int(name)
+if name == Skye:
+	print('Skye, your final grade in AP Computer Science is 94%. You have excelled in all components of the class! I wish you continued success in the next semester of AP Computer Science!')
+else:
+	print('Cary, your final grade in AP Computer Science is 62%. This largely a result of missing projects and homework assignments. Unfourtunately, because this grade is less than 65, you will have to complete an MBA assigmenent next semester.')
+print('_'*65)
---
+++ Lesson 11/problem1/problem1.py
@@ -0,0 +1,10 @@
+print('_'*65)
+print('BrIcK BoT')
+print()
+temperature = input('What is the temperature?')
+temperature = int(temperature)
+if temperature == 12:
+	print('Itz bRIck, wear ya jacket!')
+else:
+	print('Itz not bRIck, enjoy ya day!')
+print('_'*65)
